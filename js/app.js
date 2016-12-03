19a135a89de8cbeed5df43a5de40ad23b46ff979
c00z <zcusimano@gmail.com>
2016-12-03 13:57:37 -0800

    Added click image and user alternationn

--- js/app.js
+++ js/app.js
@@ -1,30 +1,46 @@
+//storing user selections, data
 var data = {
   user1: [],
   user2: [],
   currentPlayer: 'user1'
 };
 
+//sets icons
+var xIcon = 'http://www.freeiconspng.com/uploads/close-icon-29.png';
+var oIcon = 'http://www.iconsdb.com/icons/preview/gray/circle-outline-xxl.png';
+
 
 $(document).ready(function() {
 
-  $('#board').on('click', userClick)
+  $('#board').on('click', function(event) {
+    userClick(event, data.user1);
+  })
 
-  $('.box').on('click', boxClick)
+  //initiation: calling current user status
+  setCurrentUserStatus();
+
+
+
 
 });
+
+//
+function setCurrentUserStatus() {
+  $('#current-user').text(data.currentPlayer);
+}
 
 //Checking for win
 function checkWin(selections) {
   var ret = false;
     //Wining combo array
 var winCombos = [
-  [1,2,3],
-  [1,5,9],
+  [0,1,2],
+  [0,4,8],
+  [0,3,6],
   [1,4,7],
-  [2,5,8],
-  [4,5,6],
-  [7,8,9],
-  [3,6,9]
+  [3,4,5],
+  [6,7,8],
+  [2,5,8]
 ];
 
   winCombos.forEach(function(combo){
@@ -47,17 +63,22 @@ var winCombos = [
 
 console.log(checkWin([1,2,4]));
 
-function callX() {
 
-}
+function userClick(event, user) {
+  var currentPlayer = data.currentPlayer;
+  var $element = $(event.target);
 
-function callY() {
+  var boxIndex = $('.box').index($element);
+  $element.html('<img src="' + xIcon + '"/>');
+  user.push(boxIndex);
 
-}
+  console.log('the box index is:', user );
+  console.log('have I won?', checkWin(user));
+  console.log('who is the current player?', currentPlayer);
 
-function userClick() {
-  console.log("Hello")
-
+  // switch to other user
+  data.currentPlayer = (currentPlayer === 'user1') ? 'user2' : 'user1';
+  setCurrentUserStatus();
 }
 
 function boxClick() {
