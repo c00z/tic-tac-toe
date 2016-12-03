dfae0667f9d559caa3a5e3226737eaa5cdcae56c
c00z <zcusimano@gmail.com>
2016-12-03 10:50:43 -0800

    Individual button click and some styling

--- js/app.js
+++ js/app.js
@@ -1,4 +1,29 @@
 console.log('javascript is working');
 $(document).ready(function() {
 
+  $('#board').on('click', userClick)
+
+  $('.box').on('click', boxClick)
+
+
+
+
 });
+
+function callX() {
+
+}
+
+function callY() {
+
+}
+
+function userClick() {
+  console.log("Hello")
+
+}
+
+function boxClick() {
+  console.log("test")
+
+}
