e70f49a4022f576dd8cfbf3bea2f55a878b715ce
c00z <zcusimano@gmail.com>
2016-12-03 13:18:19 -0800

    Created win array and data variables

--- js/app.js
+++ js/app.js
@@ -1,14 +1,51 @@
-console.log('javascript is working');
+var data = {
+  user1: [],
+  user2: [],
+  currentPlayer: 'user1'
+};
+
+
 $(document).ready(function() {
 
   $('#board').on('click', userClick)
 
   $('.box').on('click', boxClick)
 
-
-
-
 });
+
+//Checking for win
+function checkWin(selections) {
+  var ret = false;
+    //Wining combo array
+var winCombos = [
+  [1,2,3],
+  [1,5,9],
+  [1,4,7],
+  [2,5,8],
+  [4,5,6],
+  [7,8,9],
+  [3,6,9]
+];
+
+  winCombos.forEach(function(combo){
+    var count = 0;
+    // check if 3 selections in combo
+    combo.forEach(function(num) {
+      var match = selections.indexOf(num) > -1;
+      if (match) {
+        count++;
+      }
+    });
+
+    if (count > 2) {
+      ret = true;
+    }
+  });
+
+  return ret;
+}
+
+console.log(checkWin([1,2,4]));
 
 function callX() {
 
